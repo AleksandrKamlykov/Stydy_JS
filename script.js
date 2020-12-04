405d2fa14526fe7d9031a05f49791478258babcb
Kamlykov <aleksandrkamlykov@gmail.com>
2020-12-04 18:35:45 +0200

    update programm(ciles) 1.01

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", '');
+"use stict";
+ const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -9,13 +9,27 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt("один из полседних просмотреных фильмов?", ""),
-      b = prompt("на сколько оцените его?", ""),
-      c = prompt("один из полседних просмотреных фильмов?", ""),
-      d = prompt("на сколько оцените его?", "");
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
 
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
 
-personalMovieDB.movies[a]=b;
-personalMovieDB.movies[c]=d;
+if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Произошла ошибка");
+}
 
-console.log(personalMovieDB)
+console.log(personalMovieDB);
